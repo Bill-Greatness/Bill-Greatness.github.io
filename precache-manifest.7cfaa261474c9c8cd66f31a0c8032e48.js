8bf15e5e1703a577ae2dab07fd0da170dd284fae
Bill Greatness <bill@localhost.localdomain>
2020-04-21 22:39:22 +0000

    Updates

---
+++ precache-manifest.7cfaa261474c9c8cd66f31a0c8032e48.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0b0047367276707ebf9cdde7799c5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a66158412a21e81893c0",
+    "url": "/static/css/2.7253a256.chunk.css"
+  },
+  {
+    "revision": "c6f69ef30ac2871de4e7",
+    "url": "/static/css/main.b8868e3d.chunk.css"
+  },
+  {
+    "revision": "a66158412a21e81893c0",
+    "url": "/static/js/2.fe1812a9.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.fe1812a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f69ef30ac2871de4e7",
+    "url": "/static/js/main.913994e1.chunk.js"
+  },
+  {
+    "revision": "9d3f19c6c6fc1b7052c0",
+    "url": "/static/js/runtime-main.ccea8b33.js"
+  },
+  {
+    "revision": "0c35b0de23dd9e0492d6505791c09dd1",
+    "url": "/static/media/bill-greatness.0c35b0de.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "041a69257838aa58085f08e862c8f738",
+    "url": "/static/media/ghana-health.041a6925.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "ff5fe5a7db7a9cafad17e038c16bb74a",
+    "url": "/static/media/logicians.ff5fe5a7.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "8d3022ca2e086728a706ec0d6d828327",
+    "url": "/static/media/phoenix-tech.8d3022ca.jpg"
+  }
+]);
